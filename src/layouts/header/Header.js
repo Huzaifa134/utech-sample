9bf2737d8af0dce57679d9ac4908d6c464ef5fd6
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2023-11-09 04:59:32 +0500

    about

--- src/layouts/header/Header.js
+++ src/layouts/header/Header.js
@@ -29,15 +29,15 @@ const Header = () => {
 
 
   return (
-    <header id="header" className="d-none d-xl-block">
+    <header id="header" className="d-none d-xl-block ">
       <div className="top-bar">
-        <div className="inner jus-ct">
+        <div className="inner jus-ct" style={{background:"#007aff"}} >
           <p className="clr-pri-1">
             Working Jours : Sun - Friday, 08:00 am - 07:00 pm
           </p>
         </div>
       </div>
-      <div id="site-header" className="">
+      <div id="site-header" className=""  style={{background:"#00a3e8"}}>
         <div className="container-fluid">
           <div className="row">
             <div className="col-md-12">
@@ -45,7 +45,13 @@ const Header = () => {
                 <div id="site-logo" className="clearfix">
                   <Link href="/">
                     <a className="logo">
-                      <img src="assets/images/logo/logodark.png" alt="Kinco" />
+                      {/*<img src="assets/images/logo/logodark.png" alt="Kinco" />*/}
+                      <img
+                          src="assets/images/logo/black.png"
+                          alt="kinco"
+                          height={80}
+                          width={80}  
+                        />
                     </a>
                   </Link>
                 </div>
@@ -55,36 +61,18 @@ const Header = () => {
                 <div className="nav-wrap">
                   <nav id="mainnav" className="mainnav st-2">
                     <ul className="menu">
-                      <li className="menu-item-has-children">
-                        <a href="#">HOME</a>
-                        <ul className="sub-menu">
-                          <Home />
-                        </ul>
-                      </li>
+                    <li className="menu-item-has-no-children ">
+                    <Link href="/">Home</Link>
+                      
+                    </li>
                       <li className="menu-item">
                         <About />
                       </li>
                       <li className="menu-item-has-children">
                         <a>PAGES</a>
                         <ul className="sub-menu">
-                          <li className="inner-menu-item">
-                            <a href="#">Teachers</a>
-                            <ul className="sub-menu">
-                              <Teacher />
-                            </ul>
-                          </li>
-                          <li className="inner-menu-item ">
-                            <a href="#">Classes</a>
-                            <ul className="sub-menu">
-                              <Classes />
-                            </ul>
-                          </li>
-                          <li className="inner-menu-item">
-                            <a href="#">Events</a>
-                            <ul className="sub-menu">
-                              <Event />
-                            </ul>
-                          </li>
+                          
+                          
                           <Pages />
                         </ul>
                       </li>
@@ -132,6 +120,7 @@ const Header = () => {
                       href="#"
                       className="menu-bar-right header-menu"
                       onClick={() => setSidebarToggle(true)}
+                      
                     >
                       <svg
                         data-name="Hero Area"
@@ -139,6 +128,8 @@ const Header = () => {
                         width={58}
                         height={58}
                         viewBox="0 0 58 58"
+                        
+                        
                       >
                         <defs>
                           <style
@@ -148,11 +139,12 @@ const Header = () => {
                             }}
                           />
                         </defs>
-                        <g id="Menu_Area2" data-name="Menu Area">
-                          <g id="Menu_bar" data-name="Menu bar">
+                        <g id="Menu_Area2" data-name="Menu Area" >
+                          <g id="Menu_bar" data-name="Menu bar" >
                             <path
                               id="Bg"
                               className="cls-1"
+                              
                               d="M7.853,2.283c14.9-3.89,29.969-1.4,43.467.819a7.923,7.923,0,0,1,5.735,5.422c3.111,14.141-.428,28.636-1.166,42.981a5.157,5.157,0,0,1-4.773,4.875c-13.49.568-23.463,3.285-41.787,0.9C5.948,56.807,2.348,54.2,1.9,51.7-0.683,37.877.2,23.508,2.194,8.757a8.71,8.71,0,0,1,5.66-6.473"
                             />
                             <path
@@ -185,9 +177,9 @@ const Header = () => {
                           </g>
                         </g>
                       </svg>
-                      <ul>
-                        <li className="clr-pri-2">Hotline</li>
-                        <li className="clr-pri-2">+012 (345) 678</li>
+                      <ul >
+                        <li className="clr-pri-2 text-white">Hotline</li>
+                        <li className="clr-pri-2 text-white">+012 (345) 678</li>
                       </ul>
                     </div>
                   </div>
