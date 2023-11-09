6321cdb4b02016db378a6673a49bae443bf9eb55
Muhammad Huzaifa <mhuzaifa134@gmail.com>
2023-11-09 14:33:58 +0500

    font link

--- pages/_app.js
+++ pages/_app.js
@@ -23,6 +23,12 @@ const App = ({ Component, pageProps }) => {
           href="https://fonts.googleapis.com/css2?family=Salsa&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Salsa&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {loader && <PreLoader />}
       <Component {...pageProps} />
